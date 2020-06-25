01c50076c632f27e4d4403da9bf8ca9eab8709be
josiah <josiahdamiwilliams@gmail.com>
2020-06-25 15:53:19 +0100

    Rendering app

---
+++ react-gameoflife/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import GridContainer from "./js/components/GridContainer";
+import RulesContainer from "./js/components/RulesContainer";
+import Title from "./js/components/Title";
+// import About from './components/About'
+
+function App() {
+  return (
+    <div className="app_container">
+      <Title />
+      <div className="mid_section_container">
+        <GridContainer />
+        <RulesContainer />
+      </div>
+      {/* <About /> */}
+    </div>
+  );
+}
+
+export default App;
